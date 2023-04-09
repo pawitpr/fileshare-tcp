e593d11357efc778218a5284a14ee1a038eb4582
PawitSahare <123424956+pawitpr@users.noreply.github.com>
2023-04-10 04:17:28 +0530

    error fixed
    error fixed

--- client.js
+++ client.js
@@ -3,8 +3,8 @@ const net = require('net');
 const progress = require('progress');
 const { performance } = require('perf_hooks');
 
-const PORT = 5000;
-const FILE_PATH = 'download/file.bin'; // Update with your desired file path to save
+const PORT = 5001;
+const FILE_PATH = 'share/file.bin'; // Update with your desired file path to save
 
 const fileSize = fs.statSync(FILE_PATH).size; // Get file size
 const progressBar = new progress('Downloading [:bar] :percent :etas', {
@@ -18,7 +18,7 @@ let lastBytesRead = 0;
 let lastUpdateTime = performance.now();
 
 const fileStream = fs.createWriteStream(FILE_PATH);
-const socket = net.connect(PORT, '0.0.0.0', () => {
+const socket = net.connect(PORT, 'serveo.net', () => {
   console.log('Connected to server.');
 
   socket.on('data', (data) => {
@@ -38,14 +38,4 @@ const socket = net.connect(PORT, '0.0.0.0', () => {
   socket.on('error', (err) => {
     console.error('Socket error:', err);
   });
-
-  setInterval(() => {
-    const now = performance.now();
-    const timeElapsed = now - lastUpdateTime;
-    const bytesRead = socket.bytesRead;
-    const speed = ((bytesRead - lastBytesRead) / 1024 / timeElapsed) * 1000; // Convert to KB/s
-    lastBytesRead = bytesRead;
-    lastUpdateTime = now;
-    console.log(`Network speed: ${speed.toFixed(2)} KB/s`);
-  }, 1000);
 });
